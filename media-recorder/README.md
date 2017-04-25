dbbd72ebdb8f34c328ea91b667bbbc3bd6d9b1cb
Nick Repetti <nickrepetti@gmail.com>
2017-04-24 21:33:33 -0400

    Added a play button to play back the video. Also disabled the buttons during appropriate states.

--- media-recorder/README.md
+++ media-recorder/README.md
@@ -4,13 +4,13 @@ The purpose of this app is to allow users to record video from their webcams.
 
 This app uses the getUserMedia API to record video from a user's webcam.
 Google Chrome only allows use of the API through secure origins only.
-See: https://www.chromium.org/Home/chromium-security/deprecating-powerful-features-on-insecure-origins
+See: https://www.chromium.org/Home/chromium-security/deprecating-powerful-features-on-insecure-origins.
 
 Because of this, the app must be run through HTTPS or localhost.
 I have included a static http file server in order to run the app locally.
 
 To run the app:
 1. cd to the top level directory
-2. Run the command "node static-server.js"
+2. Run the command "node js/static-server.js"
 3. Open a web browser and go to localhost:9000/index.html
 
--- media-recorder/js/app.js
+++ media-recorder/js/app.js
@@ -9,15 +9,30 @@
   if (hasGetUserMedia)  {
     const recordButton = document.getElementById("recBtn");
     const stopButton = document.getElementById("stopBtn");
+    const playButton = document.getElementById("playBtn");
 
-    let recordedChunks = [];
+    let videoElement = document.getElementById("videoElement");
+
+    let recordedChunks;
     let mediaRecorder;
 
     stopButton.addEventListener("click", (event) => {
+      stopButton.disabled = true;
+      playButton.disabled = false;
+
       mediaRecorder.stop();
       mediaRecorder.stream.getTracks().forEach((track) => {
         track.stop();
       });
+    });
+
+    playButton.addEventListener("click", (event) => {
+      recordButton.disabled = false;
+
+      const buffer = new Blob(recordedChunks);
+      videoElement.src = window.URL.createObjectURL(buffer);
+      videoElement.play();
+      videoElement.controls = true;
     });
 
     recordButton.addEventListener("click", (event) => {
@@ -28,8 +43,12 @@
       navigator.getUserMedia(
         options, 
         (stream) => {
-          let videoElement = document.getElementById("videoElement");
+          recordButton.disabled = true;
+          stopButton.disabled = false;
+          playButton.disabled = true;
+
           videoElement.src = window.URL.createObjectURL(stream);
+          recordedChunks = [];
 
           mediaRecorder = new MediaRecorder(stream);
           mediaRecorder.start();
