6b4751bc5ba5bc2524bc32f464643edb4f42e8cb
Nick Repetti <nickrepetti@gmail.com>
2017-04-26 22:47:14 -0400

    Initial work on adding a pause button

--- media-recorder/js/app.js
+++ media-recorder/js/app.js
@@ -1,65 +1,79 @@
 (function() {
   "use strict";
 
-  const hasGetUserMedia = (navigator.getUserMedia ||
-                           navigator.webkitGetUserMedia ||
-                           navigator.mozGetUserMedia ||
-                           navigator.msGetUserMedia);
+  const nav = navigator;
 
-  if (hasGetUserMedia)  {
-    const recordButton = document.getElementById("recBtn");
-    const stopButton = document.getElementById("stopBtn");
+  if (nav.getUserMedia || nav.webkitGetUserMedia || nav.mozGetUserMedia || 
+      nav.msGetUserMedia) {
+
+    let recordButton = document.getElementById("recBtn");
+    let stopButton = document.getElementById("stopBtn");
     let videoElement = document.getElementById("videoElement");
-    let recordedChunks;
+
+    let capturedData = [];
     let mediaRecorder;
 
     const captureData = function(event) {
       if (event.data.size > 0) {
-        recordedChunks.push(event.data);
+        capturedData.push(event.data);
       }
     };
 
-    const finishRecording = function(event) {
-      const buffer = new Blob(recordedChunks);
-      videoElement.src = window.URL.createObjectURL(buffer);
-      videoElement.play();
-      videoElement.controls = true;
-    };
-
-    stopButton.addEventListener("click", (event) => {
-      recordButton.disabled = false;
-      stopButton.disabled = true;
-
-      mediaRecorder.stream.getTracks().forEach((track) => {
-        track.stop();
-      });
-
-      mediaRecorder.stop();
-    });
-
-    recordButton.addEventListener("click", (event) => {
+    const startRecording = function() {
       const options = {
         video: true
       };
- 
-      navigator.getUserMedia(
+
+      nav.getUserMedia(
         options, 
         (stream) => {
-          recordButton.disabled = true;
-          stopButton.disabled = false;
-
           videoElement.src = window.URL.createObjectURL(stream);
-          recordedChunks = [];
 
           mediaRecorder = new MediaRecorder(stream);
           mediaRecorder.ondataavailable = captureData;
-          mediaRecorder.onstop = finishRecording;
+          mediaRecorder.onstop = stopRecording;
           mediaRecorder.start(1000);
         },
         (err) => {
           console.log(err);
         }
       );
+    };
+
+    const stopRecording = function(event) {
+      mediaRecorder.stream.getTracks().forEach((track) => {
+        track.stop();
+      });
+
+      const buffer = new Blob(capturedData);
+      videoElement.src = window.URL.createObjectURL(buffer);
+      videoElement.play();
+      videoElement.controls = true;
+      capturedData = [];
+    };
+
+    stopButton.addEventListener("click", (event) => {
+      recordButton.disabled = false;
+      stopButton.disabled = true;
+
+      mediaRecorder.stop();
+    });
+
+    recordButton.addEventListener("click", (event) => {
+      stopButton.disabled = false;
+
+      if (recordButton.innerText === "Record") {
+        recordButton.innerText = "Pause";
+
+        if (capturedData.size > 0) {
+          mediaRecorder.resume();
+        } else {
+          startRecording();
+        }
+      } else if (recordButton.innerText === "Pause") {
+        recordButton.innerText = "Record";
+        mediaRecorder.pause();
+      }
     });
   } else {
     console.log("Browser does not support getUserMedia");
